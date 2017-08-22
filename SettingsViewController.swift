f2c86c066eb56ecb699e21a9b619e6e6830e7556
fcbayer <30780935+fcbayer@users.noreply.github.com>
2017-08-21 21:32:48 -0700

    Add files via upload

---
+++ SettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsViewController.swift
+//  Tip Tip
+//
+//  Created by Gautam Rao on 8/20/17.
+//  Copyright © 2017 Gautam Rao. All rights reserved.
+//
+
+import UIKit
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tipSettings: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+        
+    
+}
